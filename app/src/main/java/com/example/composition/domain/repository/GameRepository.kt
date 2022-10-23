e2f2b295640a4e37e73f81a3e64c6a71940bd55d
dinmuxammedmamanov <dimamanov37@gmail.com>
2022-10-23 14:27:33 +0500

    nothing

--- app/src/main/java/com/example/composition/domain/repository/GameRepository.kt
+++ app/src/main/java/com/example/composition/domain/repository/GameRepository.kt
@@ -5,8 +5,6 @@ import com.example.composition.domain.entity.Level
 import com.example.composition.domain.entity.Question
 
 interface GameRepository {
-
     fun generateQuestion(maxSumValue: Int, countOfOptions: Int): Question
-
     fun getGameSettings(level: Level): GameSettings
 }
